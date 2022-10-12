4eb14916cc02b8245a0ea31054c417cd1917fd01
José Alvarez <jol.alvarezc@gmail.com>
2022-10-12 16:36:25 -0300

    Feature/File Storage (#6)
    * [WIP] File Storage
    
    * File storage methods
    =

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,45 @@
-# WIP
+#!/usr/bin/python3
+"""
+File Storage
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    File storage WIP
+    """
+    __file_path = "file.json"
+    __objects = {}
+    
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        k = "{}.{}".format(obj.__class__.__name__, obj.id)
+        # k = f"{self.obj.__class__.__name__}.{obj.id}"
+        self.__objects[k] = obj
+        
+    def save(self):
+        """
+        Function that writes an object to text file,
+        using a JSON representation.
+        """
+        dic ={}
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            for k, v in self.__objects.items():
+                dic[k] = v.to_dict()
+            json.dump(dic, f)
+
+    def reload(self):
+        """Reloading"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                d = json.load(f)
+                for v in d.values():
+                    val = v['__class__']
+                    del v['__class__']
+                    self.new(eval(val)(**v))
+        except FileNotFoundError:
+            pass
